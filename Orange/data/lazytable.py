2e4f0ba6dad07c6a2f01ed27fc161941cabb6728
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2014-05-28 15:35:14 +0200

    First attempt to support X, Y, metas. What is the __len__ of half a table?

--- Orange/data/lazytable.py
+++ Orange/data/lazytable.py
@@ -3,31 +3,32 @@ LazyTable is a data structure derived from Table that doesn't
 necessarily have all the data it represents directly available.
 
 TODO:
-- Harmonize naming to the Orange naming convention.  
+- Harmonize naming to the Orange naming convention.
+- Think of how to use unique keys. The row_index should work, but this should
+  then also be stored somewhere because X, Y and metas do not have to have
+  the correct order. That is, if a widget first asks for self.data[10]
+  and subsequently for self.data[5], then the first row in X will be row 10
+  and the second row 5 etc.
 """
 
-from Orange.data.table import RowInstance, Table, Columns
+from Orange.data.table import Instance, RowInstance, Table
 
-class LazyRowInstance(RowInstance):
+#class LazyRowInstance(RowInstance):
+class LazyRowInstance(Instance):
     """
     LazyRowInstance is a lazy version of RowInstance.
     
     This is a very early rudimentary version.
     
     TODO:
-    - Add support for X, Y, metas.
-    - So we can use __init__ of super()
     - Go through the 'table' to get individual cells instead of going
-      directly to the widgit_origin.
+      directly to the widget_origin.
     """
     def __init__(self, table, row_index):
         """
         Construct a data instance representing the given row of the table.
         """
-        # Can't call super().__init__() because RowInstance uses
-        # X and Y of the table, which is not yet supported.
-        # TODO: Add support for X, Y etc.
-        #super().__init__(table.domain)
+        super().__init__(table.domain)
         self.table = table
         self.row_index = row_index
     
@@ -36,10 +37,18 @@ class LazyRowInstance(RowInstance):
         Returns a specific value by asking the table / widget_origin
         for the value.
         
-        TODO: Do not go to widget_origin direcly, but go to the table
+        TODO:
+        - Do not go to widget_origin directly, but go to the table
           first. The table should then cache the value in it's X/Y.
+        - Check whether value is already available based on a unique key (row_index?).
+        - Cache the data under a unique key if not yet available.
         """
-        return self.table.widget_origin.pull_cell(self.row_index, key.name)
+        value = self.table.widget_origin.pull_cell(self.row_index, key)
+        # TODO: where does the 'int' come from?
+        if isinstance(value, int):
+            value = float(value)
+        self[key] = value
+        return value
         
     
 
@@ -64,16 +73,80 @@ class LazyTable(Table):
       do not have to be exact, but this depends on the reason why
       the stats are requested.
     """
-    
+
     widget_origin = None
-    
+
     def __getitem__(self, index_row):
         row = LazyRowInstance(self, index_row)
+
+        # Ensure that the row is added to X and Y etc.
+        # TODO: allow rows to have not all their attributes filled in.
+        for k in self.domain:
+            value = row[k]
+
+        self.append(row)
         return row
 
-    def __len__(self):
+    def len_full_data(self):
+        """
+        Returns the full length of the dataset. Not all this data might be initialized.
+        This length can be unknown, if the full data set has not yet been derived.
+        This length can also be infinite, in case an infinite generator is used to create
+        this laze table.
+        """
         length = self.widget_origin.pull_length()
+        print("in len_full_data!", length)
         return length
+
+    def len_instantiated_data(self):
+        """
+        Returns the length of the instantiated data. This is the data that is directly
+        available to the widgets. The rest of the data can still be requested by accessing
+        it though.
+        """
+        length = len(self.X)
+        print("in len_instantiated_data!", length)
+        return length
+
+    take_len_of_instantiated_data = False
+    def __len__(self):
+        """
+        There are two versions of len(), one to get the size of the dataset irrespective of how much of it is
+        already available in python and one to get the size of the available data.
+
+        The append() and insert() functions below are used to add newly instantiated rows to the already
+        instantiated data. These should use the instantiated data length and not the full one.
+        """
+        length = self.len_instantiated_data() if self.take_len_of_instantiated_data else self.len_full_data()
+        return length
+
+    def fake_len_old(self, func, *args, **kwargs):
+        """
+         This does not work for some reason.
+        """
+        self.__len__ = self.len_instantiated_data
+        return_value = func(*args, **kwargs)
+        self.__len__ = self.len_full_data
+        return return_value
+
+    def fake_len(self, func, *args, **kwargs):
+        """
+        This is a bit of a kludge.
+        """
+        should_we_revert_take_len = (self.take_len_of_instantiated_data == False)
+        self.take_len_of_instantiated_data = True
+        return_value = func(*args, **kwargs)
+        if should_we_revert_take_len:
+            self.take_len_of_instantiated_data = False
+        return return_value
+
+
+    def append(self, *args, **kwargs):
+        return self.fake_len(super().append, *args, **kwargs)
+
+    def insert(self, *args, **kwargs):
+        return self.fake_len(super().insert, *args, **kwargs)
+
 
     def has_weights(self):
         """
