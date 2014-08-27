884e790534822e6eb61d16c824c784c89bd4dc3b
David Williams <david@david-williams.info>
2014-08-27 11:58:43 +0200

    Tidying up code.

--- Orange/classification/decision_tree.py
+++ Orange/classification/decision_tree.py
@@ -5,18 +5,18 @@ from collections import defaultdict
 
 
 def calculate_entropy(instance_ids):
+
+    # Compute the frequency of each class.
     frequencies = defaultdict(int)
     for instanceID in instance_ids:
         c = lenses_table[instanceID].get_class()
-        class_string = c.variable.str_val(c)
-        frequencies[class_string] += 1
-    
-  #for cls, freq in frequencies.items():
-    #print(cls, freq)
-    
+        frequencies[str(c)] += 1
+    #print(frequencies)
+
+    # Compute the entropy
     entropy = 0
     for cls, freq in frequencies.items():
-        prob = float(freq) / float(len(lenses_table))
+        prob = float(freq) / float(len(instance_ids))
         entropy -= prob*log2(prob)
   
     return entropy
@@ -27,9 +27,8 @@ def split_data(instance_ids, attribute):
   
     for instance_id in instance_ids:
         attr_val = lenses_table[instance_id][attribute]
-        attr_string = attr_val.variable.str_val(attr_val)
-        tables[attr_string].append(instance_id)
-    
+        tables[str(attr_val)].append(instance_id)
+
     return tables
 
 
@@ -51,18 +50,18 @@ def get_attributes():
     
     return attributes
   
+if __name__ == "__main__":
+    lenses_table = Table(r"lenses.tab")
+    all_instance_ids = []
+    all_instance_ids.extend(range(0, len(lenses_table)))
+    original_entropy = calculate_entropy(all_instance_ids)
+    print('Original entropy = ', original_entropy)
 
-lenses_table = Table(r"lenses.tab")
-all_instance_ids = []
-all_instance_ids.extend(range(0, len(lenses_table)))
-original_entropy = calculate_entropy(all_instance_ids)
-print('Original entropy = ', original_entropy)
-
-possibleAttributes = get_attributes()
-for attributeToTest in possibleAttributes:
-    tables = split_data(all_instance_ids, attributeToTest)
-    states = attribute_states(attributeToTest)
-    newEntropy = 0
-    for state in states:
-        newEntropy += calculate_entropy(tables[state]) * (float(len(tables[state]) / len(lenses_table)))
-    print('Entropy after splitting on', attributeToTest, '=', newEntropy)
+    possibleAttributes = get_attributes()
+    for attributeToTest in possibleAttributes:
+        tables = split_data(all_instance_ids, attributeToTest)
+        states = attribute_states(attributeToTest)
+        newEntropy = 0
+        for state in states:
+            newEntropy += calculate_entropy(tables[state]) * (float(len(tables[state]) / len(lenses_table)))
+        print('Entropy after splitting on', attributeToTest, '=', newEntropy)
