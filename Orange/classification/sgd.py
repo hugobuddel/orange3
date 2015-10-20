521485f45a813d4674485d8c71de0ac9039cd775
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2015-10-20 14:39:44 +0200

    Iterate 5 times for partial_fit to mimic n_iter=5.

--- Orange/classification/sgd.py
+++ Orange/classification/sgd.py
@@ -12,13 +12,27 @@ class SGDLearner(Orange.classification.SklLearner):
         self.reset()
 
     def fit(self, X, Y, W):
-        return SGDClassifier(self.clf.partial_fit(X, Y.reshape(-1), self.all_classes))
+        self.clf = self.clf.partial_fit(X, Y.reshape(-1), self.all_classes)
+        # Fit some more times, see comment in partial_fit.
+        return self.partial_fit(X, Y, W)
 
     def partial_fit(self, X, Y, W):
-        return SGDClassifier(self.clf.partial_fit(X, Y.reshape(-1)))
+        for i in range(5):
+            self.clf = self.clf.partial_fit(X, Y.reshape(-1))
+        
+        # Fit 5 times because n_iter is set to 1 for partial_fit.
+        # See help of SGDClassifier:
+        """
+        n_iter : int, optional
+
+        The number of passes over the training data (aka epochs). The number
+        of iterations is set to 1 if using partial_fit. Defaults to 5.
+        """
+        return SGDClassifier(self.clf)
 
     def reset(self):
-        self.clf = linear_model.SGDClassifier(loss='log') # 'log' or 'modified_huber' required to predict probabilities.
+        # 'log' or 'modified_huber' required to predict probabilities.
+        self.clf = linear_model.SGDClassifier(loss='log')
 
 class SGDClassifier(Orange.classification.SklModel):
 
