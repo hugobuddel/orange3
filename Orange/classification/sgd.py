e1beb9a180988f81fad8b73f5a20a8c017ca550e
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2015-10-30 15:31:40 +0100

    Debug statement.

--- Orange/classification/sgd.py
+++ Orange/classification/sgd.py
@@ -27,7 +27,35 @@ class SGDLearner(Orange.classification.SklLearner):
                 X -= self.means
             if self.stds is not None:
                 X /= self.stds
-        
+
+        # Sometimes this exception occurs, but it is unclear when:
+        """
+        Exception in thread Thread-3:
+        Traceback (most recent call last):
+          File "/home/evis/anaconda3/lib/python3.4/threading.py", line 920, in _bootstra
+        p_inner
+            self.run()
+          File "/home/evis/anaconda3/lib/python3.4/threading.py", line 868, in run
+            self._target(*self._args, **self._kwargs)
+          File "/home/evis/orange3/Orange/widgets/classify/owsgd.py", line 239, in train
+        ing_thread
+            self.train()
+          File "/home/evis/orange3/Orange/widgets/classify/owsgd.py", line 261, in train
+            classifier = self.learner.partial_fit(new_instances.X, new_instances.Y, None)
+          File "/home/evis/orange3/Orange/classification/sgd.py", line 31, in partial_fit
+            self.clf = self.clf.partial_fit(X, Y.reshape(-1), self.all_classes)
+          File "/home/evis/anaconda3/lib/python3.4/site-packages/sklearn/linear_model/stochastic_gradient.py", line 526, in partial_fit
+            coef_init=None, intercept_init=None)
+          File "/home/evis/anaconda3/lib/python3.4/site-packages/sklearn/linear_model/stochastic_gradient.py", line 358, in _partial_fit
+            X, y = check_X_y(X, y, 'csr', dtype=np.float64, order="C")
+          File "/home/evis/anaconda3/lib/python3.4/site-packages/sklearn/utils/validation.py", line 450, in check_X_y
+            _assert_all_finite(y)
+          File "/home/evis/anaconda3/lib/python3.4/site-packages/sklearn/utils/validation.py", line 52, in _assert_all_finite
+            " or a value too large for %r." % X.dtype)
+        ValueError: Input contains NaN, infinity or a value too large for dtype('float64').
+        """
+        #print("SGD training to %s" % Y.reshape(-1))
+
         self.clf = self.clf.partial_fit(X, Y.reshape(-1), self.all_classes)
         
         return SGDClassifier(self.clf)
